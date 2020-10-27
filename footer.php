7ef3e333510d9ca3c6814b6cc0f8404a99e1a522
Hichem Chabou <41421720+Hichem-Chabou@users.noreply.github.com>
2020-10-27 18:19:39 +0100

    Initial upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+    </div> <!-- /.container -->
+
+		<footer class="blog-footer">
+      <p>Blog 2020&#169;</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer> 
+
+<?php wp_footer(); ?> 
+  </body>
+</html>
